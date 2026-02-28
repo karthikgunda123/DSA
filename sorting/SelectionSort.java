1ce6292dccc55715b484e6201acc784c57685d43
Karthik Gunda <gundakarthik644@gmail.com>
2026-02-28 17:05:01 +0530

    Completed Selection Sort problem

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            array[i] = scanner.nextInt();
+        }
+        selectionSort(array);
+        for (int num: array)
+            System.out.print(num + " ");
+    }
+    public static void selectionSort(int[] array){
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++){
+            int minIndex = i;
+            for (int j = i; j < n; j++){
+                if (array[j] < array[minIndex])
+                    minIndex = j;
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
